4de51b940a3f2ecc7373a589be6b1686c1130ed7
marek <amigomarek@gmail.com>
2017-12-13 12:54:54 +0100

    fully working game version 1.0

--- app/src/main/java/com/example/marek/connect3game/MainActivity.java
+++ app/src/main/java/com/example/marek/connect3game/MainActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +15,18 @@ public class MainActivity extends AppCompatActivity {
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
     int activePlayer = 0;
+    boolean activeGame = true;
 
     public void dropIn (View view) {
         ImageView counter = (ImageView) view;
         //counter.getTag(0);
         Log.i("Our tag test", counter.getTag().toString());
 
+
+
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        if(gameState[tappedCounter]==2 && activeGame) {
         gameState[tappedCounter] = activePlayer;
         counter.setTranslationY(-1500);
 
@@ -41,10 +48,39 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     winner = "Red";
                 }
-                Toast.makeText(this, winner + " has won", Toast.LENGTH_SHORT).show();
+                activeGame = false;
+//                Toast.makeText(this, winner + " has won", Toast.LENGTH_SHORT).show();
+                Button playAgainButton = findViewById(R.id.playAgainButton);
+                TextView winnerTextView = findViewById(R.id.winnerTextView);
+                winnerTextView.setText(winner + " has won");
+                playAgainButton.setVisibility(View.VISIBLE);
+                winnerTextView.setVisibility(View.VISIBLE);
 
             }
         }
+        }
+    }
+
+    public void playAgain(View view) {
+        Button playAgainButton = findViewById(R.id.playAgainButton);
+        TextView winnerTextView = findViewById(R.id.winnerTextView);
+
+        playAgainButton.setVisibility(View.INVISIBLE);
+        winnerTextView.setVisibility(View.INVISIBLE);
+
+        GridLayout gridLayout = findViewById(R.id.gridLayoutId);
+
+        for(int i=0; i<gridLayout.getChildCount(); i++) {
+            ImageView counter = (ImageView) gridLayout.getChildAt(i);
+            counter.setImageDrawable(null);
+        }
+
+        for (int i = 0; i <gameState.length; i++) {
+            gameState[i]=2;
+        }
+
+        activePlayer = 0;
+        activeGame = true;
     }
 
     @Override
