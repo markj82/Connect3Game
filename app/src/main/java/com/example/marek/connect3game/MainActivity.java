315eb55414b3527ffb4d2a8c260628835878a99c
marek <amigomarek@gmail.com>
2017-12-13 09:34:13 +0100

    adding activePlayer to MainActivity.java

--- app/src/main/java/com/example/marek/connect3game/MainActivity.java
+++ app/src/main/java/com/example/marek/connect3game/MainActivity.java
@@ -7,10 +7,20 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 0 - yellow player, 1 - red player
+    int activePlayer = 0;
+
     public void dropIn (View view) {
         ImageView counter = (ImageView) view;
         counter.setTranslationY(-1500);
-        counter.setImageResource(R.drawable.yellowbutton);
+
+        if (activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellowbutton);
+            activePlayer = 1;
+        } else {
+            counter.setImageResource(R.drawable.redbutton);
+            activePlayer = 0;
+        }
         counter.animate().translationYBy(1500).setDuration(300);
     }
 
